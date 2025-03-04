da589010540c58a5189ac25b0ee66f94d076f8fa
taubatexasrobotica@gmail.com <guisamuel800@gmail.com>
2025-03-04 17:11:21 -0300

    chore: redefine robotpy version

--- Drivetrain.py
+++ Drivetrain.py
@@ -1,27 +1,28 @@
 import wpilib
 import wpilib.drive
 import phoenix5
+import constants
 
 class Drivetrain:
     def __init__(self):
         # Initialize motors using CAN IDs
-        self.left_front_motor = phoenix5.WPI_VictorSPX(4)
-        self.left_rear_motor = phoenix5.WPI_VictorSPX(3) 
-        # self.left_rear_motor.setInverted(True)
+        self.left_front_motor = phoenix5.WPI_VictorSPX(constants.LEFT_FRONT_ID)
+        self.left_back_motor = phoenix5.WPI_VictorSPX(constants.LEFT_BACK_ID)
+        # self.left_back_motor.setInverted(True)
 
-        self.right_front_motor = phoenix5.WPI_VictorSPX(1)
-        self.right_rear_motor = phoenix5.WPI_VictorSPX(2)
+        self.right_front_motor = phoenix5.WPI_VictorSPX(constants.RIGHT_FRONT_ID)
+        self.right_back_motor = phoenix5.WPI_VictorSPX(constants.RIGHT_BACK_ID)
 
         # Group left and right motors
-        self.left = wpilib.MotorControllerGroup(self.left_front_motor, self.left_rear_motor)
-        self.right = wpilib.MotorControllerGroup(self.right_front_motor, self.right_rear_motor)
+        self.left = wpilib.MotorControllerGroup(self.left_front_motor, self.left_back_motor)
+        self.right = wpilib.MotorControllerGroup(self.right_front_motor, self.right_back_motor)
 
         self.right.setInverted(True)
 
         # Setup differential drive for arcade drive
         self.robot_drive = wpilib.drive.DifferentialDrive(self.left, self.right)
-        self.r_encoder = wpilib.Encoder(4, 5)
-        self.l_encoder = wpilib.Encoder(2, 3)
+        self.r_encoder = wpilib.Encoder(constants.RIGHT_ENCODER_A, constants.RIGHT_ENCODER_B)
+        self.l_encoder = wpilib.Encoder(constants.LEFT_ENCODER_A, constants.LEFT_ENCODER_B)
 
         wpilib.SmartDashboard.putNumber("Encoder Left", 0)
         wpilib.SmartDashboard.putNumber("Encoder Right", 0)
@@ -33,7 +34,9 @@ class Drivetrain:
         self.right_pulses = self.r_encoder.get()
         self.left_position = self.left_pulses / self.pulsos_p_m_l
         self.right_position = self.right_pulses / self.pulsos_p_m_r
-        
+        print("RIGHT: " + str(self.right_pulses))
+        print("LEFT: " + str(self.left_pulses))
+
         wpilib.SmartDashboard.putNumber("Encoder Left", self.left_pulses)
         wpilib.SmartDashboard.putNumber("Encoder Right", self.right_pulses)
 
