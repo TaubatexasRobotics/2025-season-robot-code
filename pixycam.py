fbcc9b40da7992720dba9cf4545a12935ea36146
Guilherme Samuel <guisamuel800@gmail.com>
2025-03-06 10:36:32 -0300

    feat: create getDistance function that converts bytes to readable numbers

--- pixycam.py
+++ pixycam.py
@@ -5,4 +5,9 @@ class PixyCam:
     def __init__(self) -> None:
         self.arduino = wpilib.SerialPort(constants.BAUD_RATE, wpilib.SerialPort.Port.kUSB1)
     
-    def readBytes(self, port) ->
+    def getDistance(self) -> float:
+        if self.arduino.getBytesReceived() <= 0:
+            return -1
+        buffer = bytearray(self.arduino.getBytesReceived())
+        sz = self.arduino.read(buffer)
+        return float(buffer[:sz].decode("ascii"))
