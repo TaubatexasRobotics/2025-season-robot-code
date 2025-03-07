3bf5512e1c10e7566dcde65310937ae8e34634b8
Guilherme Samuel <guisamuel800@gmail.com>
2025-03-07 16:49:33 -0300

    feat: create function that get yaw from specfic tag

--- camera.py
+++ camera.py
@@ -7,9 +7,20 @@ class AprilTagCamera:
     def __init__(self, camera: str) -> None:
         self.camera = photonlibpy.PhotonCamera(camera)
 
-    def getBestTarget(self) -> Optional[PhotonTrackedTarget]:
-        result = self.camera.getLatestResult()
-        if result.hasTargets():
-            target = result.getBestTarget()
-            return target
-        return None
+	def getBestTarget(self) -> Optional[PhotonTrackedTarget]:
+		result = self.camera.getLatestResult()
+		if result.hasTargets():
+			target = result.getBestTarget()
+			return target
+		return None
+
+    def getYaw(self, tag) -> float:
+        target_yaw = 0
+        target_range = 0
+        results = self.camera.getAllUnreadResults()
+        if len(results) > 0:
+            result = results[-1]
+            for target in result.getTargets():
+                if target.getFiducialId() == tag:
+                    return target.getYaw()
+        return -1
